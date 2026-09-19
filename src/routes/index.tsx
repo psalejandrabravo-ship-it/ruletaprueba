7c4cbef53ec5384a8b76c0f7e57f74c5b751c584
psalejandrabravo-ship-it <ps.alejandrabravo@gmail.com>
2026-09-18 23:18:59 -0300

    Elimina la ruta de TanStack Start

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { App } from "@/app/App";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <App />;
-}
